ccf5207006aba1e154b7317793df46266558ceea
AlexMTP <alexander.maestro@mtp.es>
2021-11-12 16:45:32 +0100

    Commit con el test delenium y los checks

---
+++ check_test.py
@@ -0,0 +1,45 @@
+import time
+import win32api
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/checkboxes")
+
+checkbox1= driver.find_element(By.CSS_SELECTOR,"#checkboxes > input[type=checkbox]:nth-child(1)")
+checkbox2= driver.find_element(By.CSS_SELECTOR,"#checkboxes > input[type=checkbox]:nth-child(3)")
+
+def marcar_checks(check1, check2):
+
+    if check1:
+        if checkbox1.is_selected()
+
+
+    if check2:
+
+
+
+
+   pass
+
+
+try:
+    marcar_checks(True, True)
+    win32api.MessageBox(0, 'Clickando checkboxes', 'true y true')
+
+    marcar_checks(True, False)
+    win32api.MessageBox(0, 'Clickando checkboxes', 'true y false')
+
+    marcar_checks(False, True)
+    win32api.MessageBox(0, 'Clickando checkboxes', 'false y true')
+
+    marcar_checks(False, False)
+    win32api.MessageBox(0, 'clickando checkboxes', 'false y false')
+
+
+finally:
+    time.sleep(5)
+    driver.quit()
+
--- testanimeflv.py
+++ testanimeflv.py
@@ -1,4 +1,17 @@
+import time
+import win32api
+
 from selenium import webdriver
-from selenium.webdriver.common.By import By
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome()
+
+try:
+    driver.get("https://www3.animeflv.net/")
+    link_serie = driver.find_element(By.ID,"")
+
+    win32api.MessageBox(0,'Nuevo episodio', 'Alerta')
+finally:
+    time.sleep(10)
+    driver.quit()
 
 
